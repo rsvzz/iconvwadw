f9ad3e7575805678d71b4590af6812502b8b9024
rsvzz <ronald_soft88@outlook.com>
2025-05-14 22:40:07 -0600

    name application changed

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char* argv[])
 {
-  auto app = Gtk::Application::create("io.github.rsvzz.icviewadw");
+  auto app = Gtk::Application::create("io.github.rsvzz.iconvwadw");
   return app->make_window_and_run<App>(argc, argv);
 }
 
